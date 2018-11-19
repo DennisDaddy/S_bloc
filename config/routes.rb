837d45dd78309148678dbf2e7dfab7a893d6bcd1
DennisDaddy <softteez95@gmail.com>
2018-11-19 09:19:26 +0300

    Generate controller posts

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+  get 'posts/index'
+
+  get 'posts/new'
+
+  get 'posts/show'
+
+  get 'posts/edit'
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
