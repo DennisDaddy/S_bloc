10e390a20ded0aaad90e97635b16986c60e7294b
DennisDaddy <softteez95@gmail.com>
2018-11-19 09:26:36 +0300

    Addd root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,8 @@
 Rails.application.routes.draw do
-  get 'posts/index'
 
-  get 'posts/new'
+  root 'posts#index'
 
-  get 'posts/show'
-
-  get 'posts/edit'
-
-
+  resources :posts
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
