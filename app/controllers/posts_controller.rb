6f9cadb376a19e9aa4816869fb6da92b5f8c015e
DennisDaddy <softteez95@gmail.com>
2018-11-19 15:33:26 +0300

    Enable post creation using post request

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,9 +3,16 @@ class PostsController < ApplicationController
   end
 
   def new
+    @post =Post.new
   end
 
   def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to @post
+    else
+      render 'new'
+    end    
   end
 
   def show
@@ -18,5 +25,11 @@ class PostsController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+  
+  def post_params
+    params.require(:post).permit(:title, :content)
   end
 end
